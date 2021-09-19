2ece88ab70ffd306380804fa6846dcadaf1604d6
blakepow <blakepow@gmail.com>
2021-09-18 20:09:26 -0600

    main assignment navigation page

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
-'this is a test'
+let lastMod = () => {
+    let lastMod = `Last Modified on ${document.lastModified}`;
+    document.getElementById("last-mod").innerHTML = lastMod;
+    
+    // wut
+    document.getElementById("copyright").innerHTML += ` ${new Date().getFullYear}`;
+}
