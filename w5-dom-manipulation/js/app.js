a11e61ec26d0309420a3bca6d0f71e558a793d6d
blakepow <blakepow@gmail.com>
2021-10-14 23:51:03 -0600

    w5 activity

---
+++ w5-dom-manipulation/js/app.js
@@ -0,0 +1,13 @@
+
+const print = () => {
+    // const input = document.getElementById('favchap').value;
+    // const list = document.getElementById('list').innerHTML;
+
+    const listItem = document.createElement("LI");
+    const textnode = document.createTextNode(listItem.value);
+    listItem.appendChild(textnode);
+    document.getElementById("list").appendChild(node);
+
+    
+    
+}
