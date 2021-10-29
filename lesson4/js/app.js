1db6b5aa877a18e8b7236093b0712e8ced24d8b6
blakepow <blakepow@gmail.com>
2021-10-29 12:03:36 -0600

    lesson 4 progress

--- lesson4/js/app.js
+++ lesson4/js/app.js
@@ -1,6 +1,9 @@
-let lastMod = () => {
-    let lastMod = `Last Modified on ${document.lastModified}`;
-    document.getElementById("last-mod").innerHTML = lastMod;
-    
-    // document.getElementById("copyright").innerHTML += ` ${new Date().getFullYear}`;
-}
+const date = new Date();
+document.getElementById("current-date").innerHTML = date.toLocaleString('default', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'});
+
+const ul = document.getElementById('nav-list');
+const menuItem = document.getElementById('dropdown');
+
+menuItem.addEventListener('click', () => {
+    ul.classList.toggle('show');
+});
