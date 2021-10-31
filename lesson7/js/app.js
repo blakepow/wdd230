c3a734770202157b3db041c27d4023ff62ba7447
blakepow <blakepow@gmail.com>
2021-10-30 22:49:49 -0600

    lesson 7

---
+++ lesson7/js/app.js
@@ -0,0 +1,10 @@
+let links = document.getElementById('navbar');
+console.log(links);
+// for (let i = 0; i < links.length; i++) {
+//     console.log('hi');
+//     links[i].addEventListener("click", function() {
+//         const current = document.getElementsByClassName("active");
+//         current[0].className = current[0].className.replace(" active", "");
+//         this.className += " active";
+//     });
+// }
